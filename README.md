573795128076853599ed49222c008fa3f9181b21
Friedrich Stoltzfus <friedrichstoltzfus@gmail.com>
2025-10-07 18:35:08 -0400

    add homebrew

---
+++ README.md
@@ -0,0 +1,3 @@
+## Homebrew Tap for Rainaissance
+
+This is the homebrew tap for https://rainaissance.app.
---
+++ rainaissance.rb
@@ -0,0 +1,16 @@
+cask "rainaissance" do
+  version "1.1.4"
+  sha256 "62807c37584798fc8f1a296a13fe3bcdf2ab82db2c2d8b657bdb2683d953cf58"
+
+  url "https://github.com/freepicheep/rainaissance-releases/archive/refs/tags/#{version}.tar.gz"
+  name "Rainaissance"
+  desc "Customizable rain on your mac with glorious splashes"
+  homepage "https://rainaissance.app"
+
+  app "Rainaissance.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.freepicheep.rainaissance.plist",
+    "~/Library/Application Support/Rainaissance",
+  ]
+end

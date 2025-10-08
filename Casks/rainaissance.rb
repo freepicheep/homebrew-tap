6e975623bb9e9c08912ee4447e251bdf6508fefd
Friedrich Stoltzfus <friedrichstoltzfus@gmail.com>
2025-10-07 21:04:35 -0400

    change url to use the dmg file

--- Casks/rainaissance.rb
+++ Casks/rainaissance.rb
@@ -2,7 +2,7 @@ cask "rainaissance" do
   version "1.1.4"
   sha256 "62807c37584798fc8f1a296a13fe3bcdf2ab82db2c2d8b657bdb2683d953cf58"
 
-  url "https://github.com/freepicheep/rainaissance-releases/archive/refs/tags/v.#{version}.tar.gz"
+  url "https://github.com/freepicheep/rainaissance-releases/releases/download/v.#{version}/Rainaissance.#{version}.dmg"
   name "Rainaissance"
   desc "Customizable rain on your mac with glorious splashes"
   homepage "https://rainaissance.app"

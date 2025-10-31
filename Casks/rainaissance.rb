d1dce4b13b51c1f58756a710501c5e1f550eafa8
Friedrich Stoltzfus <friedrichstoltzfus@gmail.com>
2025-10-31 20:29:55 +0100

    update to v.1.1.7

--- Casks/rainaissance.rb
+++ Casks/rainaissance.rb
@@ -1,6 +1,6 @@
 cask "rainaissance" do
-  version "1.1.5"
-  sha256 "6e016af457523b4ff0ae8f2b2220d1a2d1bc9a8524c8fbcfe606892e116c0149"
+  version "1.1.7"
+  sha256 "9e71659384b46ac266b5571ddc005483fd2e8355bfbcf37b9a24d37446bc37db"
 
   url "https://github.com/freepicheep/rainaissance-releases/releases/download/v.#{version}/Rainaissance.#{version}.dmg"
   name "Rainaissance"

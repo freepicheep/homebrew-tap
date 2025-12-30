b36972f2f68cf6839a556e04c8cf16ca41361682
Friedrich Stoltzfus <friedrichstoltzfus@gmail.com>
2025-12-30 13:27:38 -0500

    update rainaissance cask code

--- Casks/rainaissance.rb
+++ Casks/rainaissance.rb
@@ -2,15 +2,19 @@ cask "rainaissance" do
   version "2.1.1"
   sha256 "6cb1cfef63d02843bda1a8932e08dd56a6a2353ec507e8da0e5d5073897c818c"
 
-  url "https://github.com/freepicheep/rainaissance-releases/releases/download/v#{version}/Rainaissance.#{version}.dmg"
+  url "https://github.com/freepicheep/rainaissance-releases/releases/download/v#{version}/Rainaissance.#{version}.dmg",
+      verified: "github.com/freepicheep/rainaissance-releases/"
   name "Rainaissance"
-  desc "Customizable rain and snow on your mac. Works everywhere."
+  desc "Customizable rain and snow on your mac"
   homepage "https://rainaissance.app"
+
+  depends_on macos: ">= :sonoma"
 
   app "Rainaissance.app"
 
   zap trash: [
     "~/Library/Caches/com.freepicheep.Rainaissance",
-    "~/Library/Containers/com.freepicheep.Rainaissance"
+    "~/Library/Containers/com.freepicheep.Rainaissance",
+    "~/Library/Preferences/com.freepicheep.Rainaissance.plist",
   ]
 end
